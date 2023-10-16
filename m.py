88b6937733384a883398ecf8b15ceaa506d393a4
BAKYA-0203 <bhackiyasivasuppiramaniyan@gmail.com>
2023-10-16 12:27:06 +0530

    first commit

---
+++ m.py
@@ -0,0 +1,9 @@
+import calendar
+cal = calendar.month(2002, 11)
+print("Here is the calendar:")
+print(cal)
+
+import calendar
+call = calendar.month(2003, 3)
+print("Here is the calendar:")
+print(call)
